fd3273b7827162dc3b491214d96112ecc60ce58c
Shumilkin Gena <shumilkin.gena@yandex.ru>
2014-04-12 01:58:22 +0400

    Rails down

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plentry::Application.config.secret_token = '3e28e3355e53afa981f8977376c1c743740e6c110758a018a300b25cf431d2544012b2e23c7f8455597f869748e37d04c6828bdc0371f24b647ae37b46f5ac60'
