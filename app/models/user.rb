4c7d990c7bb1e7ce113612a69f62ed157ec05740
Gena Shumilkin <shumilkin.gena@yandex.ru>
2014-04-17 11:33:13 +0400

    Add model User

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ActiveRecord::Base
+  has_secure_password
+  attr_accessible :name, :email, :password, :password_comfirmation
+
+  validates :password, presence: true,
+                      on: :create,
+                      length: {minimum: 6, maximum: 32}
+  validates :email, presence: true,
+                    uniqueness: true,
+                    email: true
+  validates :name, presence: true
+  validate :check_authenticate, if: :email
+
+  def to_s
+    name
+  end
+
+  private
+
+  def check_authenticate
+    if self.try(:authenticate, password)
+      errors.add(:password, :user_or_password_invalid)
+    end
+  end
+end
